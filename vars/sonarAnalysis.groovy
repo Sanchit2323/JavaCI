1fc23bc3a5184566a7fcb6d83038f39d4b3a76a8
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-10 03:18:35 +0530

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,7 @@
 def call() {
     stage('Static Analysis with SonarQube') {
         script {
-            withSonarQubeEnv('sonar') {
+            withSonarQubeEnv('SonarQube') {
                 sh "${tool 'maven'}/bin/mvn clean package -Dmaven.test.skip=true sonar:sonar"
             }
         }
